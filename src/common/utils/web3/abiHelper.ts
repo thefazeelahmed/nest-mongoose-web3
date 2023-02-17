56cab63cc73b0702f39bb91c71c07d6022d786a4
Fazeel Ahmed <thefazeelahmed@gmail.com>
2023-02-17 15:47:38 +0500

    erc721 contract added

--- src/common/utils/web3/abiHelper.ts
+++ src/common/utils/web3/abiHelper.ts
@@ -1,9 +1,12 @@
-import CHRG from 'contracts/CHRG.json';
+import ERC20 from 'contracts/ERC20.json';
+import ERC721 from 'contracts/ERC721.json';
 
 export const getContractABI = (contractType) => {
   switch (contractType) {
-    case 'CHRG':
-      return CHRG.abi;
+    case 'ERC20':
+      return ERC20.abi;
+    case 'ERC721':
+      return ERC721.abi;
   }
 };
 
@@ -11,7 +14,11 @@ export const getContractAddress = async (web3, contractType) => {
   const networkId = await web3.eth.net.getId();
 
   switch (contractType) {
-    case 'CHRG':
-      return CHRG.networks[networkId] ? CHRG.networks[networkId].address : '';
+    case 'ERC20':
+      return ERC20.networks[networkId] ? ERC20.networks[networkId].address : '';
+    case 'ERC721':
+      return ERC721.networks[networkId]
+        ? ERC721.networks[networkId].address
+        : '';
   }
 };
